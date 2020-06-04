66fc98b5bc9d628dc5c4482c2676cfdce5cca302
joer9514 <joer9514@gmail.com>
2020-06-03 19:11:32 -0500

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+read file task
+"""
+
+
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, mode="r", encoding='utf-8') as f:
-        info = f.read()
-    print(info, end='')
+    """Open a file and print its text inside"""
+    with open(filename, encoding='UTF-8') as file:
+        print(file.read(), end='')
